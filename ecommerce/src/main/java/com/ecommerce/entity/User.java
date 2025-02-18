82f2efc1df026669ab6fb6ea1d5ec9430ff69753
nguyennhattruong202 <nhattruongnguyen202@gmail.com>
2025-02-18 14:30:42 +0700

    config entity and connect database

---
+++ ecommerce/src/main/java/com/ecommerce/entity/User.java
@@ -0,0 +1,36 @@
+package com.ecommerce.entity;
+
+import com.ecommerce.enums.Role;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "login", nullable = false, length = 155)
+    private String login;
+    @Basic(optional = false)
+    @Column(name = "password", nullable = false, length = 155)
+    private String password;
+    
+    private Role role;
+}
