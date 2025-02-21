0e32bc1cb7b7bc9c998a8cd01f85fcf8801deadb
nguyennhattruong202 <nhattruongnguyen202@gmail.com>
2025-02-21 14:30:01 +0700

    configure product and order basket entities

--- ecommerce/src/main/java/com/ecommerce/entity/Product.java
+++ ecommerce/src/main/java/com/ecommerce/entity/Product.java
@@ -1,5 +1,66 @@
 package com.ecommerce.entity;
 
-public class Product {
-    
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Basic;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Basic(optional = false)
+    @Column(name = "title", nullable = false, length = 155)
+    private String title;
+    @Basic(optional = true)
+    @Column(name = "alias", nullable = true, length = 255)
+    private String alias;
+    @Basic(optional = true)
+    @Column(name = "description", nullable = true)
+    private String description;
+    @Basic(optional = false)
+    @Column(name = "price", nullable = false)
+    private Integer price;
+    @Basic(optional = true)
+    @Column(name = "image", length = 255, nullable = true)
+    private String image;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vendor_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ToString.Exclude
+    private Vendor vendor;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ToString.Exclude
+    private Category category;
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JsonIgnore
+    private List<OrderBasket> orderBaskets;
 }
