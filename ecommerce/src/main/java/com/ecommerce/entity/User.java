d3f57cd40dca77812e2d064eb183b651bf390568
nguyennhattruong202 <nhattruongnguyen202@gmail.com>
2025-03-01 17:05:51 +0700

    fix: add more user entity relationship

--- ecommerce/src/main/java/com/ecommerce/entity/User.java
+++ ecommerce/src/main/java/com/ecommerce/entity/User.java
@@ -2,18 +2,24 @@ package com.ecommerce.entity;
 
 import com.ecommerce.enums.Role;
 import jakarta.persistence.Basic;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "user")
@@ -40,4 +46,12 @@ public class User implements Serializable {
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
     private Role role;
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private UserInfo userInfo;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<OrderBasket> orderBaskets;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
 }
